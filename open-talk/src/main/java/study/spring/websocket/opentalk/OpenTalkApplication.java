21a88f5bfad6e3ed4b912d009ac50e8d40827803
YoungChulShin <go1323@gmail.com>
2024-08-18 19:23:01 +0900

    프로젝트 초안

---
+++ open-talk/src/main/java/study/spring/websocket/opentalk/OpenTalkApplication.java
@@ -0,0 +1,13 @@
+package study.spring.websocket.opentalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTalkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OpenTalkApplication.class, args);
+  }
+
+}
